d71fc3305d091af0b7bbd51b1c63df72493c5dcb
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2023-12-26 23:58:48 -0300

    Fundamentos Next.js e React
    ​
    Contém os seguintes tópicos:
    . Configuração do Projeto
    . O que é Next.js?
    . Estrutura de Pastas
    . Primeiro Componente & Rotas
    . Entendendo o JSX
    . Integração JavaScript/JSX

---
+++ fundamentos/src/app/page.tsx
@@ -0,0 +1,9 @@
+//import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Fundamentos de Next.js & React</h1>
+    </div>
+  )
+}
---
+++ fundamentos/src/pages/jsx.tsx
@@ -0,0 +1,13 @@
+﻿export default function Jsx() {
+  const titulo = <h1>JSX é um conceito central</h1>
+
+  function subtitulo() {
+    return <h2>{"muito legal".toUpperCase()}</h2>
+  }
+  return (
+    <div>
+      {titulo}
+      {subtitulo()}
+    </div>
+  )
+}
