951c2b2908f4dc5523bae3d7335e3aeeef7f7f21
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2024-01-01 19:04:16 -0300

    Fundamentos Next.js e React
    ​
    Tópicos:
    . Integração JavaScript/JSX

--- fundamentos/src/pages/jsx.tsx
+++ fundamentos/src/pages/jsx.tsx
@@ -1,5 +1,6 @@
 ﻿export default function Jsx() {
   const titulo = <h1>JSX é um conceito central</h1>
+  const color = "#12313"
 
   function subtitulo() {
     return <h2>{"muito legal".toUpperCase()}</h2>
@@ -8,6 +9,9 @@
     <div>
       {titulo}
       {subtitulo()}
+      <p>
+        {JSON.stringify({nome: 'João', idade: 30})}
+      </p>
     </div>
   )
 }
