801df770c9c08190ec58afdd3cf8dec01e564d0f
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2024-05-22 23:13:57 -0300

    Fundamentos Next.js e React
    Tópicos:
    . ^Navegando entre Componentes

--- fundamentos/src/pages/estiloso.tsx
+++ fundamentos/src/pages/estiloso.tsx
@@ -1,9 +1,11 @@
 ﻿import styles from "./styles/Estiloso.module.css"
+import Link from "next/link" 
 
 export default function Estiloso() {
 
   return (
     <div className={styles.roxo}>
+      <Link href="/" className={styles.roxo}>Voltar</Link>
       <h1>Estilo usando CSS Módulos</h1>
     </div>
   )
